f54a465d33c6b9d767c8de18d4510ae3c9901f5b
Natalie <natalierg1199@gmail.com>
2024-07-19 00:14:22 -0500

    homework update

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,7 +3,9 @@ import os
 import csv
 
 #create a path to CSV file
-csvpath = os.path.join("C:/Users/nwf91/anacoda3.1/envs/python_challenge/python_challenge/PyBank/Resources/budget_data.csv")
+# csvpath = os.path.join('Resources', 'budget_data.csv')
+## @GRADER: I kept getting errors using a relative path so I switched to using an abolute file path
+csvpath = "C:\\Users\\nwf91\\OneDrive\\Desktop\\VandyDataCourse\\Modules\\python-challenge\\python_challenge\\PyBank\\Resources\\budget_data.csv"
 
 #list to store data 
 dates = []
@@ -11,7 +13,7 @@ prof_loss = []
 change_profloss = []
 
 #open the csv file and create reader that understands ' as delimiter 
-with open(csvpath, encoding='UTF-8') as csvfile: 
+with open(csvpath) as csvfile: 
     csvreader = csv.reader(csvfile, delimiter=",")
     
     #skipping header row
@@ -19,25 +21,43 @@ with open(csvpath, encoding='UTF-8') as csvfile:
     
     #reading the file and adding values to list
     for row in csvreader: 
-        
+        # print(row)
         dates.append(row[0])
         prof_loss.append(row[1])
         prof_loss = list((map(int, prof_loss)))
         
-        
-# print(prof_loss)
+
+# Initialize variables for change calculation
+
+total_change = 0
 
 #use len to find number of months 
-total_num_months = len(dates)
+total_months = len(dates)
 # print(total_num_months)
+previous_profit_loss = profit_loss[0]
 
-#finding net total amount of profit/losses, first set var to 0, then define it as sum of prof/loss values
-#learned that i have to use sum() because prof_loss is a list from Xpert Learning Assistant
-net_prof_loss = 0
-net_prof_loss += sum(prof_loss)
-# print(net_prof_loss)
+# Calculate the changes in Profit/Losses
+for profit_loss in profit_loss[1:]:  # Start from the second month
+    change = profit_loss - previous_profit_loss
+    total_change += change
+    previous_profit_loss = profit_loss
 
-#finding avg change
+# Calculate the average change
+average_change = total_change / (total_months - 1)  
+print(f"Average Change is {average_change}. Total months is {total_months}. Total Change is {total_change}")
+#Greatest Increase in Profits 
+
+# Greatest Decrease in Profits 
+
+# ## The Net Total Amount of Profit/Losses over the entire period 
+# net_profloss = 0
+# net_profloss += sum(prof_loss)
+# print(net_profloss)
+
+## Average Change 
+avg_change = net_profloss/total_num_months
+print(avg_change)
+
 # for element in prof_loss: 
 #         change_profloss = element =+ element -1 
     
