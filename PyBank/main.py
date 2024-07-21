fbba066e09a1b8fd5b8746f9a5489633c5b2004f
Natalie <natalierg1199@gmail.com>
2024-07-21 16:17:46 -0500

    finished PyBank file

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,17 +2,17 @@
 import os
 import csv
 
-#create a path to CSV file
+# create a path to CSV file
 # csvpath = os.path.join('Resources', 'budget_data.csv')
-## @GRADER: I kept getting errors using a relative path so I switched to using an abolute file path
+# I kept getting errors using a relative path so I switched to using an abolute file path
 csvpath = "C:\\Users\\nwf91\\OneDrive\\Desktop\\VandyDataCourse\\Modules\\python-challenge\\python_challenge\\PyBank\\Resources\\budget_data.csv"
 
-#list to store data 
+# List to store data 
 dates = []
 prof_loss = []
-change_profloss = []
+changes = []
 
-#open the csv file and create reader that understands ' as delimiter 
+# Open the csv file and create reader that understands ' as delimiter 
 with open(csvpath) as csvfile: 
     csvreader = csv.reader(csvfile, delimiter=",")
     
@@ -26,47 +26,42 @@ with open(csvpath) as csvfile:
         prof_loss.append(row[1])
         prof_loss = list((map(int, prof_loss)))
         
-# Initialize variables for change calculation
-
-total_change = 0
-
-#use len to find number of months 
+# The total number of months included in the dataset
 total_months = len(dates)
+# print(round(total_months,2))
 
-print(total_months)
-previous_profit_loss = prof_loss[0]
+# The net total amount of "Profit/Losses" over the entire period
+total = sum(prof_loss)
+# print(round(total,2))
 
+# Average change 
+for i in range(1, len(prof_loss)):
+    change = prof_loss[i] - prof_loss[i-1]
+    changes.append(change)
 
+average_change = sum(changes)/len(changes)
+# print(round(average_change, 2))
 
-# Calculate the changes in Profit/Losses
-for profit_loss in prof_loss[1:]:  # Start from the second month
-    change = profit_loss - previous_profit_loss
-    total_change += change
-    previous_profit_loss = profit_loss
+# Greatest Increase/Decrease in Profits 
+max_increase = max(changes)
+max_increase_date = dates[changes.index(max_increase) + 1]  # Adding 1 to get the corresponding date
 
-# Calculate the average change
-average_change = total_change / (total_months - 1)  
-print(f"Average Change is {average_change}. Total months is {total_months}. Total Change is {total_change}")
-#Greatest Increase in Profits 
+max_decrease = min(changes)
+max_decrease_date = dates[changes.index(max_decrease) + 1]  # Adding 1 to get the corresponding date
+# print(f'The greatest increase was {max_increase} on {max_increase_date}the greatest decrease was {max_decrease} on {max_decrease_date}')
 
-# Greatest Decrease in Profits 
+# Print and export to text file 
+file_path = "C:\\Users\\nwf91\\OneDrive\\Desktop\\VandyDataCourse\\Modules\\python-challenge\\python_challenge\\PyBank\\PyBankresults.txt"
 
-# ## The Net Total Amount of Profit/Losses over the entire period 
-# net_profloss = 0
-# net_profloss += sum(prof_loss)
-# print(net_profloss)
-
-## Average Change 
-avg_change = net_profloss/total_num_months
-print(avg_change)
-
-# for element in prof_loss: 
-#         change_profloss = element =+ element -1 
-    
-# this is where i got lost and ran out of time. I will be resubmitting with finished version of homework asap 
-
-
-
-
-
-
+with open(file_path, 'w') as file: 
+    file.write(
+    f"""
+    Financial Analysis
+    ----------------------------
+    Total Months: {total_months}
+    Total: ${total}
+    Average Change: ${average_change:.2f}
+    Greatest Increase in Profits: {max_increase_date} (${max_increase})
+    Greatest Decrease in Profits: {max_decrease_date} (${max_decrease})
+    """
+    )
